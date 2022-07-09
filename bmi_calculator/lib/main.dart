653bbb6046a1f923b015b619817e45c12c0d7d2f
PRAVIN MOHAN SURYAWANSHI <spravin20032001@gmail.com>
2022-07-09 12:37:34 +0530

    BMI calculator
    A calculator to calculate body mass index.

---
+++ bmi_calculator/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() {
+  runApp(const BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: AppBarTheme(color: Color(0xFF090C22)),
+        scaffoldBackgroundColor: Color(0xFF090C22),
+      ),
+    );
+  }
+}
+
